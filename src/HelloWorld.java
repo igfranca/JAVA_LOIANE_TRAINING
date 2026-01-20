0177402ac88ab701161159eacb5f3d6987ce5fae
Igor Franca <igrfranca@gmail.com>
2026-01-20 13:59:15 -0300

    Second Commit

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-
-    String hello = "Hello World!";
-
-    System.out.println(hello);
-
-  }
-}
---
+++ src/basico/HelloWolrd.java
@@ -0,0 +1,9 @@
+package basico;
+
+public class HelloWolrd {
+  public static void main(String[] args) {
+
+    String helloWorld = "Hello World!!!";
+    System.out.println(helloWorld);
+  }
+}
