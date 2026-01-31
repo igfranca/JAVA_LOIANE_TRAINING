6dd9cc58b0b443dc8589769cdae0d04bbf18fa9b
Igor Franca <igrfranca@gmail.com>
2026-01-31 13:51:21 -0300

    Teste

--- src/basico/HelloWorld.java
+++ src/basico/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
   public static void main(String[] args) {
 
     String helloWorld = "Hello World!!!";
+    String helloWorld = "HELLOOOo TESTE!!!!";
     System.out.println(helloWorld);
   }
 }
